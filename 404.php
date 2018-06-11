1d91bc37fee2265df0f7b04a7f850665f624fcd2
Nitesh.Patare <Nitesh.Patare@RapidCircle.com>
2018-06-11 18:12:15 +0530

    404 changed

--- 404.php
+++ 404.php
@@ -8,6 +8,9 @@
 <center><h2>Page not Found!</center></h2><br>
 <center>This content is currently unavailable The page you requested cannot be displayed right now. </center>
 <center>It may be temporarily unavailable, the link you clicked on may have expired, or you may not have permission to view this page.</center>
+<center>
+	<a href='/'>To create a new Canvas click here</a>
+</center>
 </b>
 
 
