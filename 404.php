f42a888b34c4f7e63d7d79a22be0f929d649e7b9
Nitesh.Patare <Nitesh.Patare@RapidCircle.com>
2018-06-11 18:14:17 +0530

    404 text change

--- 404.php
+++ 404.php
@@ -4,15 +4,17 @@
 <title>404</title>
 <link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
-<body><br><b>
-<center><h2>Page not Found!</center></h2><br>
-<center>This content is currently unavailable The page you requested cannot be displayed right now. </center>
-<center>It may be temporarily unavailable, the link you clicked on may have expired, or you may not have permission to view this page.</center>
-<center>
-	<a href='/'>To create a new Canvas click here</a>
-</center>
-</b>
-
+<body class='not-found'><br>
+<p>
+	<b>
+		<center><h2>Page not Found!</center></h2><br><br>
+		<center>This content is currently unavailable The page you requested cannot be displayed right now. </center>
+		<center>It may be temporarily unavailable, the link you clicked on may have expired, or you may not have permission to view this page.</center><br><br>
+		<center>
+			<a href='/'>To create a new Canvas click here</a>
+		</center>
+	</b>
+<p>
 
 </body>
 </html>
