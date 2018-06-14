e62a4162bec0b7c497850f05469520b3dd19135f
Nitesh.Patare <Nitesh.Patare@RapidCircle.com>
2018-06-14 11:19:59 +0530

    async issue

--- ret.js
+++ ret.js
@@ -216,6 +216,6 @@ function getDraw()
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onload = handler;
-    xmlhttp.open("GET",bid+".xml",'async');
+    xmlhttp.open("GET",bid+".xml",false);
     xmlhttp.send();
 }
